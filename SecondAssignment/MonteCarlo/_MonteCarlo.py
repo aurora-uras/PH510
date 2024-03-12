84406a3ca748124e5e021749a55febbb4e6108fe
Aurora Uras <156242631+aurora-uras@users.noreply.github.com>
2024-03-12 13:14:49 +0000

    Changes to be committed: 	modified:   SecondAssignment/MonteCarlo/_MonteCarlo.py

--- SecondAssignment/MonteCarlo/_MonteCarlo.py
+++ SecondAssignment/MonteCarlo/_MonteCarlo.py
@@ -1,45 +1,32 @@
 # ====================================
 # Monte Carlo integration in parallel
 # ====================================
-
-
 import numpy as np
 from mpi4py import MPI
 
-MPI.Init()
 comm = MPI.COMM_WORLD()
 
 class MonteCarlo(object):
-    def __init__(self, f, a, b, x, n):
+    def __init__(self, f, a, b, x, N, nworkers, rank):
         self.MC_Integration = []
         self.f = function
         self.a = limit_a
         self.b = limit_b
         self.x = x_array
-        self.n = n
+        self.N = data
         self.nworkers = self.comm.Get_size()
         self.rank = self.comm.Get_rank()
 
-    def integration(f, a, b, x, n):
+    def integration(self):
 
-        rank = comm.Get_rank()
-        size = comm.Get_size()
-        
-        nproc = comm.Get_size()
-    
-        # The first processor is leader, so one fewer available to be a worker
-        nworkers = nproc - 1
-        
+        datapoints = self.N/self.n_workers
+
+        partial_integral = np.zeros(datapoints)
         integral = np.array(0.0,dtype = np.double)
         
-        for i in range(rank, n-1, size):
-            
-            partial_integral = partial_integral + (b-a)/n * np.sum(f(x))
-        
-        integral = com.reduce(partial_integral, MPI.SUM, 0)
-        
-        if i ==0:
-            print("result = ", integral)
-        MPI.Finalize()
-        return 
+        for i in range(datapoints):
+            x = random_x_loc[i]
+            partial_integral[i] = self.f(x)
+            partial_integral = partial_integral * (b-a)/self.N
+        return np.sum(partial_integral)
     
