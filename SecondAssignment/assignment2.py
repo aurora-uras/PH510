c97fd84461b91698cd74a1680a5e093690d6fb64
Aurora Uras <156242631+aurora-uras@users.noreply.github.com>
2024-03-19 17:01:06 +0000

    Update assignment2.py
    Modification to confirm with standards (variable n changed in upper case naming N, indention standardised, functions commented).
     Function for sampling over [-4sigma, +4sigma] range generated

--- SecondAssignment/assignment2.py
+++ SecondAssignment/assignment2.py
@@ -4,25 +4,33 @@ import MonteCarlo._MonteCarlo as MC
 from numpy.random import Generator, SeedSequence, MT19937
 from mpi4py import MPI
 
-
-
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-n = 100
-datapoints = n//size
+N = 100
+datapoints = N//size
 
 ss = SeedSequence(12345)    # Generate a sequence with intial seed 
 sworker = ss.spawn(size)    # spawn the sequence on the workers
 rangen = Generator(MT19937(sworker[rank]))   # random number generator
 
-xi = rangen.random(datapoints)
 sigma = rangen.random()
 x0 = rangen.random()
 def function(x):
+    """
+    Return a Gaussian function with mean x0 and standard deviation sigma
+    """
     return 1/(sigma*2*np.sqrt(np.pi)) * np.exp(-(x-x0)**2/(2*sigma**2))
 
-#print(rank, sigma, x0)
-monte = MC.MonteCarlo(function, n, xi)
-print("rank =", rank, "xi =", xi, "sigma =", sigma, "x0 =", x0, monte.evaluate())
+def xi(sigma):
+    """
+    Returns sampling function, this is chosen to go from -4sigma to 
+    +4sigma as 99.99% of the gaussian function is expected in this range
+    """
+
+    x = np.linspace(-4*sigma, 4*sigma, datapoints)
+    return x
+
+monte = MC.MonteCarlo(function, N, xi(sigma))
+print("rank =", rank, "xi =", xi(sigma), "sigma =", sigma, "x0 =", x0, monte.evaluate())
